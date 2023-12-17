2960ab50c8f03d798dd326efa87ca3273c3830d3
олена городюк <135028180+OlenaUser1982@users.noreply.github.com>
2023-12-17 12:55:22 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
     >
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
+          options={Object.keys({ good, neutral, bad })}
           onLeaveFeedback={onLeaveFeedback}
         />
       </Section>
